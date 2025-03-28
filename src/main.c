e83e4225b340728b71fe6ea58099736e115990b9
Tymoteusz Burak <tymoteusz.burak@uni-wuerzburg.de>
2025-03-28 18:15:08 +0100

    src/main.c: Add enroll + increase stack size to prevent overflow

--- src/main.c
+++ src/main.c
@@ -16,5 +16,14 @@ int main(void)
 	}
 	printf("PUF Initialized Successfully.\r\n");
 
+	// Enroll the PUF
+	memset(activation_code, 0, sizeof(activation_code));
+	status = PUF_Enroll(PUF, activation_code, sizeof(activation_code));
+	if (status != kStatus_Success) {
+		printf("Error: PUF enrollment failed!\r\n");
+		return -1;
+	}
+	printf("PUF Enroll successful. Activation Code created.\r\n");
+
 	return 0;
 }
