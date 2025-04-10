12f604cacf6a3f4b01e22e789299f3ceb18f4f18
Tymoteusz Burak <tymoteusz.burak@uni-wuerzburg.de>
2025-04-10 14:34:03 +0200

    src: Call functions by UUID, initial pseudo-TEE

--- application/src/bao_config.c
+++ application/src/bao_config.c
@@ -1,5 +1,7 @@
 #include "bao_config.h"
 #include <stdio.h>
+#include <string.h>
+#include "tee_internal_api.h"
 
 void (*bao_hypercall)(unsigned int, unsigned int, unsigned int) =
     (void (*)(unsigned int, unsigned int, unsigned int))BAO_HC_ADDR;
@@ -9,11 +11,90 @@ void ipc_notify(int ipc_id, int event_id)
     bao_hypercall(BAO_HC_IPC_ID, ipc_id, event_id);
 }
 
+typedef struct {
+    uint8_t      uuid[TEE_UUID_LEN];
+    TEE_Result (*handler)(void);
+} uuid_func_map_t;
+
+
+TEE_Result dummy_function_1(void){
+    return TEE_SUCCESS;
+}
+
+TEE_Result dummy_function_2(void){
+    return TEE_ERROR_GENERIC;
+}
+
+TEE_Result dummy_function_3(void){
+    return TEE_ERROR_CANCEL;
+}
+
+
+static const uuid_func_map_t function_table[] = {
+    {
+        .uuid = {
+            0x00, 0x11, 0x22, 0x33,   /* timeLow    */
+            0x44, 0x55,               /* timeMid    */
+            0x66, 0x77,               /* timeHi+ver */
+            0x88, 0x99, 0xAA, 0xBB,   /* clockSeq   */
+            0xCC, 0xDD, 0xEE, 0xFF    /* node       */
+        },
+        .handler = dummy_function_1,
+    },
+    {
+        .uuid = {
+            0x11, 0x22, 0x33, 0x44,   /* timeLow    */
+            0x55, 0x66,               /* timeMid    */
+            0x77, 0x88,               /* timeHi+ver */
+            0x99, 0xAA, 0xBB, 0xCC,   /* clockSeq   */
+            0xDD, 0xEE, 0xFF, 0x00    /* node       */
+        },
+        .handler = dummy_function_2,
+    },
+    {
+        .uuid = {
+            0x22, 0x33, 0x44, 0x55,   /* timeLow    */
+            0x66, 0x77,               /* timeMid    */
+            0x88, 0x99,               /* timeHi+ver */
+            0xAA, 0xBB, 0xCC, 0xDD,   /* clockSeq   */
+            0xEE, 0xFF, 0x00, 0x11    /* node       */
+        },
+        .handler = dummy_function_3,
+    }
+};
+
+void print_hex(const char* label, const void* data, size_t len) {
+    const uint8_t* bytes = (const uint8_t*)data;
+    printf("%s: ", label);
+    for (size_t i = 0; i < len; ++i) {
+        printf("%02X", bytes[i]);
+        if (i < len - 1) printf(":");
+    }
+    printf("\r\n");
+}
+
 void ipc_irq_handler(void)
 {
-    printf("MemReg0: %.*s \r\n", (int)MESSAGE0_SIZE, message[0]);
-    printf("MemReg1: %.*s \r\n", (int)MESSAGE1_SIZE, message[1]);
+    uint8_t   received_uuid[TEE_UUID_LEN];
+    TEE_Result result = TEE_ERROR_GENERIC;
 
-    // Inform that interrupt got handled
-    ipc_notify(0, 0);
+    /* Copy the UUID from shared memory */
+    memcpy(&received_uuid, message[0], sizeof(received_uuid));
+    print_hex("Received UUID", &received_uuid, sizeof(received_uuid));
+
+    /* Iterate and compare contents of each entry */
+    for (int i = 0; i < ARRAY_SIZE(function_table); i++) {
+        if (memcmp(&received_uuid,
+                   function_table[i].uuid,
+                   sizeof(received_uuid)) == 0) {
+            /* Call handler and capture the result */
+            printf("UUID matched entry %d; calling handler…\n", i);
+            result = function_table[i].handler();
+            /* Write the return code and inform that interrupt got handled */
+            memcpy(message[1], &result, sizeof(result));
+            ipc_notify(0,0);
+        }
+    }
+    printf("No matching UUID in table.\n");
+
 }
--- application/src/bao_config.h
+++ application/src/bao_config.h
@@ -13,12 +13,19 @@
 #define VMS_IPC_SIZE        0x1000
 
 /* Define shared memory layout */
-#define MESSAGE0_SIZE ((const size_t)(0x10)) //128-bit/16-byte - place for TEE Calls UUID
-#define MESSAGE1_SIZE ((const size_t)(VMS_IPC_SIZE - MESSAGE0_SIZE))
+#define MESSAGE0_SIZE       ((const size_t)(0x10)) //128-bit/16-byte - place for TEE Calls UUID
+#define MESSAGE0_OFFSET     VMS_IPC_BASE
 
-static char* const message[2] = {
-    (const char*)(VMS_IPC_BASE),
-    (const char*)(VMS_IPC_BASE + MESSAGE0_SIZE),
+#define MESSAGE1_SIZE       ((const size_t)(0x4))  //32-bit/4-byte - place for TEEC_Result
+#define MESSAGE1_OFFSET     MESSAGE0_OFFSET + MESSAGE0_SIZE
+
+#define MESSAGE2_SIZE       ((const size_t)(VMS_IPC_SIZE - MESSAGE0_SIZE - MESSAGE1_SIZE)) //Ugly, WIP
+#define MESSAGE2_OFFSET     MESSAGE1_OFFSET + MESSAGE1_SIZE
+
+static char* const message[3] = {
+    (const char*)(MESSAGE0_OFFSET),
+    (const char*)(MESSAGE1_OFFSET),
+    (const char*)(MESSAGE2_OFFSET),
 };
 
 extern void (*bao_hypercall)(unsigned int, unsigned int, unsigned int);
