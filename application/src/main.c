14ae0d4942293ab8a37f8fa6b71dad296489ad80
Muhammad <s473711>
2025-04-14 16:32:37 +0200

    Enrollment Implementation For ZK-Puf in Zephyr Environment

--- application/src/main.c
+++ application/src/main.c
@@ -1,8 +1,26 @@
 #include <stdio.h>
 #include "flash_handler.h"
 #include "puf_prover.h"
+#include "mbedtls/ecp.h"
 
-#define IS_WRITE_TO_FLASH_ENABLED 1
+#define ENROLLMENT_IS_UP 0
+
+int challengeSize = 16;
+
+const uint8_t c1[16] = {
+    0x3C, 0xA1, 0xF4, 0x92,
+    0x57, 0xB8, 0x0E, 0x6D,
+    0x1F, 0xA9, 0xC3, 0xE7,
+    0x74, 0x90, 0x12, 0xAD
+};
+
+const uint8_t c2[16] = {
+    0xFF, 0x00, 0xFF, 0x00,
+    0xFF, 0x00, 0xFF, 0x00,
+    0x00, 0xFF, 0x00, 0xFF,
+    0x00, 0xFF, 0x00, 0xFF
+};
+
 
 int main(void)
 {
@@ -11,6 +29,8 @@ int main(void)
     const struct flash_area *flash_area;
     const struct device *flash_dev;
     int ret;
+    puf_config_t pufConfig;
+
 
     /* Initialize flash area and device */
     ret = flash_initialize(FIXED_PARTITION_ID(STORAGE_PARTITION), &flash_area, &flash_dev);
@@ -18,17 +38,41 @@ int main(void)
         printf("Flash Initialization failed!\r\n");
         return ret;
     }
-
-    ret = PUF_Prover_Initialize(PUF,
+    
+    // Initialization of PUF
+    ret = initializeAndStartPuf(PUF,pufConfig,
                                     activation_code,
                                     PUF_ACTIVATION_CODE_SIZE,
                                     flash_area,
                                     flash_dev,
-                                    IS_WRITE_TO_FLASH_ENABLED);
-    if (ret != 0) {
-        printf("PUF Prover Initialization failed!\r\n");
+                                    ENROLLMENT_IS_UP);
+    
+    if(ret!=0){
+        printf("PUF Initialisation Failed\r\n");
         return ret;
     }
 
+    mbedtls_ecp_group grp;
+	mbedtls_ecp_point h, C;
+	
+    ret = performEnrollment(&grp,&h,&C,c1,challengeSize, c2, challengeSize, 
+                          PUF,
+                          activation_code,
+                          PUF_ACTIVATION_CODE_SIZE,
+                          flash_area,
+                          flash_dev,
+                          ENROLLMENT_IS_UP);
+
+    if(ret!=0){
+        printf("Enrollment Failed");
+        return ret;
+    }
+    else
+    {
+        printf("Enrollment Success");
+    }
+
+    pufDeinit(PUF,pufConfig); // Deinitalization Of Puf
+    
     return 0;
 }
--- application/src/puf_prover.h
+++ application/src/puf_prover.h
@@ -3,6 +3,14 @@
 
 #include "fsl_puf.h"
 #include "flash_handler.h"
+#include "mbedtls/ecp.h"
+
+
+#define NUM_RANDOM_NUMBERS 2
+#define RANDOM_NUMBER_SIZE_BYTES 32
+#define PUF_SLOT_0 0
+#define PUF_SLOT_1 1
+#define CONSTANT_FOR_H_GENERATOR 123456789
 
 /**
  * @brief Initialize the PUF prover.
@@ -27,5 +35,42 @@ int PUF_Prover_Initialize(PUF_Type *puf,
                           const struct flash_area *flash_area,
                           const struct device *flash_dev,
                           bool writeToFlash);
+
+
+int getPufKey(PUF_Type *puf,
+                          uint8_t *activation_code,
+                          size_t activation_code_size,
+                          const struct flash_area *flash_area,
+                          const struct device *flash_dev,
+                          bool writeToFlash,
+                          uint8_t key_slot,
+                          uint8_t *out_key_buffer);
+
+int initializeAndStartPuf(PUF_Type *puf,puf_config_t pufConfig,
+                          uint8_t *activation_code,
+                          size_t activation_code_size,
+                          const struct flash_area *flash_area,
+                          const struct device *flash_dev,
+                          bool writeToFlash);
+
+void pufDeinit(PUF_Type *puf,puf_config_t pufConfig);
+
+int performEnrollment(mbedtls_ecp_group *grp, mbedtls_ecp_point *h, mbedtls_ecp_point *C , const uint8_t *c1, size_t c1_size, const uint8_t *c2, size_t c2_size , 
+                          PUF_Type *puf,
+                          uint8_t *activation_code,
+                          size_t activation_code_size,
+                          const struct flash_area *flash_area,
+                          const struct device *flash_dev,
+                          bool writeToFlash);
+
+int getResponseToChallenge(const uint8_t *c, size_t c_size, mbedtls_mpi *r, PUF_Type *puf,
+                          uint8_t *activation_code,
+                          size_t activation_code_size,
+                          const struct flash_area *flash_area,
+                          const struct device *flash_dev,
+                          bool writeToFlash,int pufSlot
+                          );
+
+
 
 #endif /* PUF_PROVER_H */
